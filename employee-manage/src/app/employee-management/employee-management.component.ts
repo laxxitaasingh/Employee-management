d7c440541bad81fb74950d0cfcbf6bc8b67fe470
laxxitaasingh <laxitasingh2112@gmail.com>
2025-04-24 22:19:14 +0530

    creating main management component

---
+++ employee-manage/src/app/employee-management/employee-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-management',
+  templateUrl: './employee-management.component.html',
+  styleUrl: './employee-management.component.css'
+})
+export class EmployeeManagementComponent {
+
+}
